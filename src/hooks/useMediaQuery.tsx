96217158aeca93279603dc4ee79ae2e5ee3b7e0d
Mehdi Marouani <mehdi.marouani.developer@gmail.com>
2024-05-26 23:33:06 +0100

    build fix

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -1,16 +1,17 @@
+"use client";
 import { useLayoutEffect, useState } from "react";
 
 const useMediaQuery = (size: number) => {
-  const [match, setMatch] = useState([window.innerWidth <= size]);
+  const [match, setMatch] = useState<boolean[]>([false]);
 
   useLayoutEffect(() => {
     const listener = () => {
-      setMatch([window.innerWidth <= size]);
+      if (window) setMatch([window.innerWidth <= size]);
     };
     window.addEventListener("resize", listener);
     listener();
     return () => window.removeEventListener("resize", listener);
-  }, []);
+  }, [size]);
 
   return match;
 };
